c5ed902da94882978ed3b5f3996de8a1f78cd812
nickrmurphy <117491495+nickrmurphy@users.noreply.github.com>
2026-02-18 19:53:41 -0600

    feat: repository layer

---
+++ src/repos/challenge-repo.ts
@@ -0,0 +1,28 @@
+import { db } from "../db";
+import { challengeSchema, NewChallenge } from "../db/schema";
+
+export const challengeRepo = {
+  create: async (data: NewChallenge) => {
+    const validated = challengeSchema.parse(data);
+
+    // Delete expired challenges
+    db.deleteFrom("challenges")
+      .where("expires_at", "<", new Date().toISOString())
+      .execute()
+      .catch(() => {});
+
+    // Create new challenge
+    await db.insertInto("challenges").values(validated).execute();
+  },
+  consumeIfValid: async (id: string, vaultAddress: string) => {
+    const row = await db
+      .deleteFrom("challenges")
+      .where("id", "=", id)
+      .where("vault_address", "=", vaultAddress)
+      .where("expires_at", ">", new Date().toISOString())
+      .returning("challenge")
+      .executeTakeFirst();
+
+    return row?.challenge;
+  },
+};
---
+++ src/repos/vault-repo.ts
@@ -0,0 +1,24 @@
+import { db } from "../db/client";
+import { NewVault, vaultSchema } from "../db/schema";
+
+export const vaultRepo = {
+  create: async (data: NewVault) => {
+    const validated = vaultSchema.parse(data);
+    const { address } = await db
+      .insertInto("vault")
+      .values(validated)
+      .returning("address")
+      .executeTakeFirstOrThrow();
+
+    return address;
+  },
+  get: async (address: string) => {
+    const vault = await db
+      .selectFrom("vault")
+      .where("address", "=", address)
+      .selectAll()
+      .executeTakeFirst();
+
+    return vault;
+  },
+};
